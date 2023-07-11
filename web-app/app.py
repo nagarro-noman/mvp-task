7c8e4b4061e04563db9b08e73672fd251c2ec45f
nomannagarro <mohammad.noman@nagarro.com>
2023-07-11 16:01:07 +0530

    adding more

--- web-app/app.py
+++ web-app/app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 import os
+import boto3
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
 
 @app.route("/favicon.ico")
 def favicon():
@@ -21,9 +21,18 @@ def upload():
     if request.method == 'POST':
         file = request.files['file']
         if file:
-            filename = file.filename
-            # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            print(filename)
+            # Save the file locally
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(file_path)
+            
+            # Upload the file to S3
+            bucket_name = 'nagp-task-bucket-3163353'
+            s3 = boto3.client('s3')
+            s3.upload_file(file_path, bucket_name, file.filename)
+            
+            # Delete the local file
+            os.remove(file_path)
+
             return redirect(url_for('results'))
     return render_template('upload.html')
 
