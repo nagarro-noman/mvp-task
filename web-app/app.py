1009c8d95ff8ade22b35b94b45ce821b0b9b6a84
nomannagarro <mohammad.noman@nagarro.com>
2023-07-14 17:41:41 +0530

    write to db success

--- web-app/app.py
+++ web-app/app.py
@@ -40,12 +40,42 @@ def upload():
     return render_template('upload.html')
 
 
+def get_rds_endpoint():
+    try:
+        ssm = boto3.client('ssm')
+        response = ssm.get_parameter(Name='/rds/endpoint', WithDecryption=False)
+        rds_endpoint = response['Parameter']['Value']
+        return rds_endpoint[:-5]
+    except ClientError as e:
+        print(f"Error retrieving RDS endpoint from Parameter Store: {str(e)}")
+        raise e
+
 @app.route('/result')
 def results():
     # file_list = os.listdir(app.config['UPLOAD_FOLDER'])
     # print(f"result = {file_list}")
-    file_list = ["hetst", "afsd"]
-    return render_template('results.html', files=file_list)
+    rds_host = get_rds_endpoint()
+    db_name = "mydatabase"
+    username = "admin"
+    password = "password"
+    print(f"HOST name : {rds_host}")
+    try:
+        conn = pymysql.connect(host=rds_host, user=username, passwd=password, db=db_name)
+        with conn.cursor() as cursor:
+            query = "SELECT File_Name, No_Of_Letters FROM file_record"
+            cursor.execute(query)
+            
+            # Fetch all the rows from the result set
+            rows = cursor.fetchall()
+
+        logger.info("Data read from RDS successfully.")
+    except Exception as e:
+        logger.error(f"Error storing data in RDS: {str(e)}")
+    finally:
+        # Close the cursor and connection
+        cursor.close()
+        conn.close()
+    return render_template('results.html', files=row)
 
 
 if __name__ == '__main__':
